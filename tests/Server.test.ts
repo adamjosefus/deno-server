839a76a9fae1bc416a686e3bc6d2587f66a8281c
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2021-12-12 17:02:26 +0100

    Update Server.test.ts

--- tests/Server.test.ts
+++ tests/Server.test.ts
@@ -3,7 +3,7 @@ import { Server, ServerOptions } from "../model/Server.ts";
 
 
 Deno.test({
-    name: "Server.computeHostUrl",
+    name: "Server.computeServerHostUrl",
     fn: () => {
         const serverOptions: ServerOptions[] = [];
 
@@ -23,111 +23,87 @@ Deno.test({
 
             const expected = `http://${hostname}${port !== 80 ? `:${port}` : ''}`;
 
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some-path`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some-path/`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some-path/get-data`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some-path/get-data/`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}?query=123`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/?query=123`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some-path?query=123`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some-path/?query=123`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some-path`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some-path/`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some-path/get-data`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some-path/get-data/`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}?query=123`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/?query=123`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some-path?query=123`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some-path/?query=123`), expected);
         });
     },
 });
 
 
 Deno.test({
-    name: "Server.computeHostUrl + rootShift",
+    name: "Server.computeServerHostUrl + webRoot",
     fn: () => {
         const serverOptions: ServerOptions[] = [];
 
         const ports = [8080, 80, 1234];
         const hostnames = ['localhost', '127.0.0.1', 'my-dns.net'];
-        const rootShifts = ['some', 'some/path'];
+        const webRoots = ['some', 'some/path'];
 
         ports.forEach(port => {
             hostnames.forEach(hostname => {
-                rootShifts.forEach(rootShift => {
-                    serverOptions.push({ hostname, port, rootShift })
+                webRoots.forEach(webRoot => {
+                    serverOptions.push({ hostname, port, webRoot })
                 });
             });
         });
 
 
         serverOptions.forEach(_ => {
-            const { port, hostname, rootShift } = _;
-            const srv = new Server({ port, hostname, rootShift });
+            const { port, hostname, webRoot } = _;
+            const srv = new Server({ port, hostname, webRoot });
 
-            const expected = `http://${hostname}${port !== 80 ? `:${port}` : ''}/${rootShift}`;
+            const origin = `http://${hostname}${port !== 80 ? `:${port}` : ''}`;
+            const expected = `${origin}/${webRoot}`;
 
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some/path`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some/path/`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some/path/get-data`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some/path/get-data/`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some/path?query=123`), expected);
-            assertEquals(srv.computeHostUrl(`http://${hostname}:${port}/some/path/?query=123`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some/path`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some/path/`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some/path/get-data`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some/path/get-data/`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some/path?query=123`), expected);
+            assertEquals(srv.computeServerHostUrl(`http://${hostname}:${port}/some/path/?query=123`), expected);
         });
     },
 });
 
 
+Deno.test({
+    name: "Server.computeClientUrl",
+    fn: () => {
+        const serverOptions: ServerOptions[] = [];
 
-// Deno.test({
-//     name: "Compute HostURL II.",
-//     fn: () => {
-//         const server = new Server({ port: 8080, hostname: 'localhost' });
-//         const url = "http://localhost/some-path"
-//         const expected = "http://localhost";
+        const ports = [8080];
+        const hostnames = ['localhost'];
 
-//         assertEquals(server.computeHostUrl(url), expected);
-//     },
-// });
-
-// Deno.test({
-//     name: "Compute HostURL III.",
-//     fn: () => {
-//         const server = new Server({ port: 8080, hostname: 'localhost', hostpath: 'some-path' });
-//         const url = "http://localhost/some-path"
-//         const expected = url;
-
-//         assertEquals(server.computeHostUrl(url), expected);
-//     },
-// });
+        ports.forEach(port => {
+            hostnames.forEach(hostname => {
+                serverOptions.push({ hostname, port })
+            });
+        });
 
 
+        serverOptions.forEach(_ => {
+            const { port, hostname } = _;
+            const srv = new Server({ port, hostname });
 
-// Deno.test({
-//     name: "Compute BasePath I.",
-//     fn: () => {
-//         const server = new Server({ port: 8080, hostname: 'localhost' });
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}`), '/');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}/`), '/');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}/some-path`), '/some-path');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}/some-path/`), '/some-path');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}/some-path/get-data`), '/some-path/get-data');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}/some-path/get-data/`), '/some-path/get-data');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}?query=123`), '/?query=123');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}/?query=123`), '/?query=123');
+            assertEquals(srv.computeClientUrl(`http://${hostname}:${port}/some-path?query=123`), '/some-path?query=123');
+            // assertEquals(srv.computePublicUrl(`http://${hostname}:${port}/some-path/?query=123`), '/some-path?query=123');
+        });
+    },
+});
 
-//         const url = "http://localhost"
-//         const expected = '';
-
-//         assertEquals(server.computeBasePath(url), expected);
-//     },
-// });
-
-// Deno.test({
-//     name: "Compute BasePath II.",
-//     fn: () => {
-//         const server = new Server({ port: 8080, hostname: 'localhost' });
-//         const url = "http://localhost/some-path"
-//         const expected = "some-path";
-
-//         assertEquals(server.computeBasePath(url), expected);
-//     },
-// });
-
-// Deno.test({
-//     name: "Compute BasePath III.",
-//     fn: () => {
-//         const server = new Server({ port: 8080, hostname: 'localhost', hostpath: 'some-path' });
-//         const url = "http://localhost/some-path"
-//         const expected = '';
-
-//         assertEquals(server.computeBasePath(url), expected);
-//     },
-// });
